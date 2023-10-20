c727d847e93d690cae0dd9ca5beb39dea6575e1d
aldantas <allandantas21@gmail.com>
2023-10-20 16:47:46 -0300

    add makefile

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,10 +6,12 @@
 /*   By: aldantas <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/20 15:00:13 by aldantas          #+#    #+#             */
-/*   Updated: 2023/10/20 15:00:35 by aldantas         ###   ########.fr       */
+/*   Updated: 2023/10/20 16:20:14 by aldantas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stdio.h>
 #include <stdlib.h>
 
-
+char	*ft_strtrim(char const *s1, char const *set);
+{	
+}
