8fb39d2407401fd8da5a447b76bc718aac0f9196
AllanDantas21 <allandantas21.ad@gmail.com>
2023-10-25 00:17:08 -0300

    ft_strtrim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -10,3 +10,27 @@
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	size_t	len;
+	char	*s2;
+	char	*pos;
+
+	while (*s1 && ft_strchr(set, *s1))
+		*s1++;
+	len = ft_strlen(s1);
+	while (*s1 && ft_strchr(set, s1[len--]));
+	len += 2;
+	s2 = (char *)malloc(sizeof(char) * (len + 1));
+	if (!s2)
+		return (NULL);
+	pos = s2;
+	while (len--)
+		*s2++ = *s1++;
+	*s2 = '\0';
+	return (pos);
+}
+/*int	main()
+{
+	printf("%s", ft_strtrim("xx batata   ", "x "));
+}*/
