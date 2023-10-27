464910e5860a84c1410cfb926912d6e7bcaf58ec
aldantas <allandantas21@gmail.com>
2023-10-27 20:27:36 -0300

    fix strdup

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,23 +6,25 @@
 /*   By: aldantas <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 13:55:52 by aldantas          #+#    #+#             */
-/*   Updated: 2023/10/27 20:10:55 by aldantas         ###   ########.fr       */
+/*   Updated: 2023/10/27 20:27:09 by aldantas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
-char	*ft_strdup(char *s1)
+char	*ft_strdup(const char *s1)
 {	
+	char	*str1;
 	char	*s2;
 	char	*pos;
-
+	
+	str1 = (char *)s1;
 	s2 = (char *)malloc(sizeof(char) * (ft_strlen(s1) + 1));
 	if (!s2)
 		return (NULL);
 	pos = s2;
 	while (*s1)
 	{
-		*s2++ = *s1++;
+		*s2++ = *str1++;
 	}
 	*s2 = 0;
 	return (pos);
