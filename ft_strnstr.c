6364577ea0feb2807c8a6fa93b483a3161a2a8b3
Allan Dantas do Nascimento <135276676+AllanDantas21@users.noreply.github.com>
2023-10-29 21:14:22 -0300

    change strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -11,26 +11,28 @@
 /* ************************************************************************** */
 #include "libft.h"
 
-char	*ft_strnstr(const char *big, const char *little, size_t len)
-{	
-	int	i;
-	int	c;
+char    *ft_strnstr(const char *big, const char *little, size_t len)
+{
+        size_t  i;
+        size_t  c;
 
-	i = 0;
-	c = 0;
-	if (little[i] == '\0')
-		return ((char *)big);
-	if (len == 0)
-		return (NULL); 
-	while ((big[i] != '\0' && little[c] != '\0') && len--)
-	{
-		if (big[i] == little[c])
-			c++;
-		else
-			c = 0;
-		i++;
-	}
-	if (little[c] == '\0')
-		return ((char *)(&big[i - c]));
-	return (0);
+        i = 0;
+        if (!big && !len)
+                return (0);
+        if (little[0] == '\0' || little == big)
+                return ((char *)big);
+        while ((big[i] != '\0'))
+        {
+                c = 0;
+                while ((big[i + c] == little[c]) && (i + c) < len)
+                {
+                        if (big[i + c] == '\0' && little[c] == '\0')
+                                return ((char *)&big[i]);
+                        c++;
+                }
+                if (little[c] == '\0')
+                        return ((char *)(&big[i]));
+                i++;
+        }
+		return (0);
 }
