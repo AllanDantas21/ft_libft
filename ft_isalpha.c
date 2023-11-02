36e4bd1fcbaa2e322141358e96dd1f7d1cee22c3
Allan Dantas do Nascimento <135276676+AllanDantas21@users.noreply.github.com>
2023-11-02 15:10:14 -0300

    Update ft_isalpha.c

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -11,7 +11,7 @@
 /* ************************************************************************** */
 #include "libft.h"
 
-int	ft_isalpha(int i)
+int	ft_isalpha(int c)
 {
-	return ((i >= 'a' && i <= 'z') || (i >= 'A' && i <= 'Z'));
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }
