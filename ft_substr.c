fc7a4f277b1d415966bb9ee5523bc3929fc4c9ab
aldantas <allandantas21@gmail.com>
2023-11-09 08:28:10 -0300

    norms fix

--- ft_substr.c
+++ ft_substr.c
@@ -13,22 +13,24 @@
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	unsigned int	s_len;
-	unsigned int	max_len;
-	char			*sub;
+	char	*str;
+	char	*pos;
+	int		lcpy;
 
 	if (!s)
 		return (NULL);
-	s_len = ft_strlen(s);
-	if (start < s_len)
-		max_len = s_len - start;
-	if (start >= s_len)
-		max_len = 0;
-	if (max_len > len)
-		max_len = len;
-	sub = (char *)malloc((max_len + 1) * sizeof(char));
-	if (sub == NULL)
+	if (start >= len)
+		return (ft_strdup(""));
+	lcpy = (int)(len);
+	str = (char *)malloc((len * sizeof(char)) + 1);
+	if (!str)
 		return (NULL);
-	ft_strlcpy(sub, s + start, max_len + 1);
-	return (sub);
+	pos = str;
+	while (lcpy > 0)
+	{
+		*str++ = (char)s[start++];
+		lcpy--;
+	}
+	*str = '\0';
+	return (pos);
 }
